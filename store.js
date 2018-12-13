0e33d1d81b2f7d6bf5ca6ef4a6e0a53d7aed5c02
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-13 10:30:37 -0500

    adds setup functions to store.js

---
+++ store.js
@@ -0,0 +1,17 @@
+const {createStore} = require('redux');
+// ===========================================
+// SETUP
+// ===========================================
+// #1 describe the state
+
+
+// #2 list out all the changes you could make to state
+
+// #3 -write action creator functions
+
+// #4 write a reducer function that accepts state and an action
+// and returns the next version of the state
+
+// #5 create a store that uses your reducer
+
+
