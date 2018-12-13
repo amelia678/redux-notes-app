0b9ed8ed10fa604de1ca93e90aca42fb8020b8fc
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-13 10:33:23 -0500

    exports store and action creator

--- store.js
+++ store.js
@@ -5,13 +5,53 @@ const {createStore} = require('redux');
 // #1 describe the state
 
 
+    const defaultState = {
+    note: {
+        content: ''
+        }
+    }
+
+
+
 // #2 list out all the changes you could make to state
 
+/*
+-UPDATE CONTENT
+*/
+
+const ACTION_UPDATE = {
+    type: 'ACTION_UPDATE',
+    content: '???????'
+}
+
+
 // #3 -write action creator functions
+const updateContent = (content) => {
+    return {
+        ...ACTION_UPDATE,
+        content
+    }
+}
 
 // #4 write a reducer function that accepts state and an action
 // and returns the next version of the state
 
-// #5 create a store that uses your reducer
+const note = (state=defaultState, action) => {
+    switch(action.type) {
+        case ACTION_UPDATE.type:
+        // do somethin
+            return {
+                note: {
+                    content: action.content
+                }
+            }
+        default:
+            return state;
+    }
 
+}
+// #5 create a store that uses your reducer
+const store = createStore(note);
+
+module.exports= store;
 
